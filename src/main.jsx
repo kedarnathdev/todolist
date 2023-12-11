9bd4ddcda736170411f715892c3cc61f889586e8
kedarnathdev <kedarnathperaka@gmail.com>
2023-12-11 12:11:57 +0530

    changes in package json

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import './index.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
