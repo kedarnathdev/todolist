0112d6c7251a5d09c6492b0058195960c49e1a9b
kedarnathdev <kedarnathperaka@gmail.com>
2023-12-12 17:13:08 +0530

    updates

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,27 +1,67 @@
 import React from 'react'
 import Navigationbar from './Navigationbar'
-import { Card, CardList, Section, SectionCard } from '@blueprintjs/core'
+import { FormGroup, InputGroup, Section, SectionCard } from '@blueprintjs/core'
+import { useAuth0 } from '@auth0/auth0-react'
+import { toast } from 'sonner';
+import { Navigate } from 'react-router-dom';
 
 const Profile = () => {
+  const {user, isAuthenticated} = useAuth0();
+  if (!isAuthenticated) {
+    return (
+      <>
+      {toast.error("login first")}
+      <Navigate to="/" />
+      </>
+    )
+  }
   return (
     <>
-    <Navigationbar />
-    <div>
-      <Section title="Profile">
-        <SectionCard padded={true}>
-          <CardList bordered={true}>
-            <Card interactive={true} onClick={() => toast.error('Deleted')}>
-              Learn JAVA
-            </Card>
-            <Card interactive={true} onClick={() => toast.error('Deleted')}>
-              Learn React
-            </Card>
-          </CardList>
-        </SectionCard>
-      </Section>
-    </div>
+      <Navigationbar />
+      <div>
+        <Section title="Profile">
+          <SectionCard padded={true}>
+            <img src={user.picture}></img>
+          <FormGroup
+            label="Email"
+          >
+            <InputGroup placeholder="email" value={user.email} disabled />
+          </FormGroup>
+          <FormGroup
+            label="Email Verified"
+          >
+            <InputGroup placeholder="Email Verified" value={user.email_verified} disabled />
+          </FormGroup>
+          <FormGroup
+            label="Family Name"
+          >
+            <InputGroup placeholder="family name" value={user.family_name} disabled />
+          </FormGroup>
+          <FormGroup
+            label="Given Name"
+          >
+            <InputGroup placeholder="given name" value={user.given_name} disabled />
+          </FormGroup>
+          <FormGroup
+            label="Locale"
+          >
+            <InputGroup placeholder="locale" value={user.locale} disabled />
+          </FormGroup>
+          <FormGroup
+            label="Name"
+          >
+            <InputGroup placeholder="name" value={user.name} disabled />
+          </FormGroup>
+          <FormGroup
+            label="Nickname"
+          >
+            <InputGroup placeholder="nickname" value={user.nickname} disabled />
+          </FormGroup>
+          </SectionCard>
+        </Section>
+      </div>
     </>
-    
+
   )
 }
 
