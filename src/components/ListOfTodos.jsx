9fa33081bc48d939b993c4fc4ab57fe7fc9bf004
kedarnathdev <kedarnathperaka@gmail.com>
2023-12-14 11:43:29 +0530

    changes in list of todos

--- src/components/ListOfTodos.jsx
+++ src/components/ListOfTodos.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { toast } from 'sonner';
-import { Card, CardList, Section, SectionCard, Alert } from '@blueprintjs/core';
+import { Card, CardList, Section, SectionCard, Alert, FormGroup, InputGroup, Button } from '@blueprintjs/core';
 const ListOfTodos = () => {
+  const [todo, setTodo] = useState('')
+  const [todoList, setTodolist] = useState([])
+  const addTodo = () => {
+    if (!todoList.includes(todo))
+    setTodolist([...todoList, todo])
+  }
+  const removeTodo = (data) => {
+    todoList.splice(todoList.indexOf(data), 1)
+    console.log(todoList)
+    setTodolist([...todoList])
+  }
   return (
     <div>
       <Section title="List of Todos">
         <SectionCard padded={true}>
+          <FormGroup
+            helperText="Todo Text"
+            label="Todo"
+            labelFor="text-input"
+            labelInfo="(required)"
+          >
+            <InputGroup id="text-input" placeholder="Enter a Todo" value={todo} onChange={(e) => setTodo(e.target.value)} />
+            <Button icon="add" onClick={addTodo}>Add</Button>
+          </FormGroup>
           <CardList bordered={true}>
-            <Card interactive={true} onClick={() => toast.error('Deleted')}>
-              Learn JAVA
-            </Card>
-            <Card interactive={true} onClick={() => toast.error('Deleted')}>
-              Learn React
-            </Card>
+            {todoList.map((data) => (
+              <Card key={data} interactive={true} onClick={()=>{
+                removeTodo(data)
+                toast.error('Deleted')
+                }}>
+                {data}
+              </Card>
+            ))}
           </CardList>
         </SectionCard>
       </Section>
